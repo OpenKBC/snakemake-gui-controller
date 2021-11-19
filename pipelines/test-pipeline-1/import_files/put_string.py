0397296164b9f72a5bee058e94459ca2ac0a04b8
Jun <swiri021@gmail.com>
2021-11-19 00:28:50 -0600

    Updated for v1.0.4 version of image

--- pipelines/test-pipeline-1/import_files/put_string.py
+++ pipelines/test-pipeline-1/import_files/put_string.py
@@ -1,8 +1,12 @@
 from sys import argv
 import pandas as pd
+import time
 
 count_start = 0
 with open(argv[1], "r") as f:
     data = f.read()
+
+time.sleep(2)
+
 with open(argv[2], "w") as rw:
     rw.write(data+("-this is test result"))
--- pipelines/test-pipeline-1/import_files/sum.py
+++ pipelines/test-pipeline-1/import_files/sum.py
@@ -1,9 +1,12 @@
 from sys import argv
+import time
 
 count_start = 0
 with open(argv[1], "r") as f:
     for line in f.readlines():
         count_start+=int(line)
+
+time.sleep(2)
 
 with open(argv[2], "w") as rw:
     rw.write(str(count_start))
